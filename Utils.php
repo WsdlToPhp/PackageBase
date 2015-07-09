821593ff54f3704843bf7b8e6e6f7154e3a8c872
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-09 12:40:24 +0200

    refactor getFormatedXml method

--- Utils.php
+++ Utils.php
@@ -14,6 +14,8 @@ class Utils
      */
     public static function getFormatedXml($string, $asDomDocument = false)
     {
+        $dom = null;
+        $formated = $string;
         if (!empty($string) && class_exists('DOMDocument')) {
             $dom = new \DOMDocument('1.0', 'UTF-8');
             $dom->formatOutput = true;
@@ -23,12 +25,12 @@ class Utils
             $dom->validateOnParse = false;
             try {
                 if ($dom->loadXML($string)) {
-                    return $asDomDocument ? $dom : $dom->saveXML();
+                    $formated = $dom->saveXML();
                 }
             } catch (\Exception $exception) {
-                throw new \InvalidArgumentException(sprintf('XML string is invalid'), null, $exception);
+                throw new \InvalidArgumentException('XML string is invalid', $exception->getCode(), $exception);
             }
         }
-        return $asDomDocument ? null : $string;
+        return $asDomDocument ? $dom : $formated;
     }
 }
