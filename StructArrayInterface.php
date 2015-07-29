a5fa2a0409070989f0102cfeadf29453d2e948ee
Mikael DELSOL <delsol.mikael@gmail.com>
2015-07-29 06:51:27 +0200

    mandevilla - add Interface for Struct and StructArray

---
+++ StructArrayInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace WsdlToPhp\PackageBase;
+
+interface StructArrayInterface extends StructInterface, \ArrayAccess, \Iterator, \Countable
+{
+    /**
+     * Method returning alone attribute name when class is *array* type
+     * This method has been overridden in real-array struct class
+     * @return string
+     */
+    function getAttributeName();
+}
